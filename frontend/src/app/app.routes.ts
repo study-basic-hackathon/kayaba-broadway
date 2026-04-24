8f280ed48c9c8cb6f52730645293de2e9071ce9d
shun144 <noelgld13@gmail.com>
2026-04-24 10:38:12 +0900

    ルートガード追加

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,17 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { FieldComponent } from './pages/field/field.component';
 import { FieldsComponent } from './pages/fields/fields.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'field', component: FieldComponent },
-  { path: 'fields', component: FieldsComponent },
+  {
+    path: '',
+    canActivate: [authGuard],
+    children: [
+      { path: 'field', component: FieldComponent },
+      { path: 'fields', component: FieldsComponent },
+    ],
+  },
 ];
