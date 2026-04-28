70e97968be8960caa61a3e168d1fde6f0fadab60
shun144 <noelgld13@gmail.com>
2026-04-28 18:56:05 +0900

    MIMEスニッフィング対応

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,11 @@ app.use(
   }),
 );
 
+app.use("/*", async (c, next) => {
+  await next();
+  c.header("X-Content-Type-Options", "nosniff");
+});
+
 const factory = createFactory<{ Bindings: Env }>();
 app.use(
   "/*",
