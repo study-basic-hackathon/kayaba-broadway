32a2348e220ae9766fa4c3d15c988096c4e00238
shun144 <noelgld13@gmail.com>
2026-05-01 21:24:03 +0900

    支払いページ処理作成

--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -4,6 +4,8 @@ import { Hono } from "hono";
 import Stripe from "stripe";
 import { user_payment_providers } from "../db/schema";
 import { type AppType, AppDatabase } from "../types";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
 type UserPaymentProvider = typeof user_payment_providers.$inferSelect;
 const router = new Hono<AppType>();
@@ -13,62 +15,76 @@ router.get("/setup", async (c) => {
   const stripe = c.get("stripe");
   const db = drizzle(c.env.DB!);
 
-  const userPaymentRecord = (await db
-    .select()
-    .from(user_payment_providers)
-    .where(eq(user_payment_providers.user_id, payload.id))
-    .get()) as UserPaymentProvider | undefined;
-
-  if (!userPaymentRecord) {
-    const customer = await createStripeCustomer(
-      stripe,
-      db,
-      payload.id,
-      payload.email,
-    );
-
-    return c.json({ customer }, 200);
-  }
-
   try {
-    const customer = await stripe.customers.retrieve(
-      userPaymentRecord.customer_id,
-    );
-    return c.json({ customer }, 200);
-  } catch (err) {
-    if (
-      err instanceof Stripe.errors.StripeInvalidRequestError &&
-      err.code === "resource_missing"
-    ) {
-      const customer = await createStripeCustomer(
-        stripe,
-        db,
-        payload.id,
-        payload.email,
-      );
-
-      return c.json({ customer }, 200);
+    const userPaymentRecord = await fetchUserPayment(db, payload.id);
+    if (userPaymentRecord) {
+      return c.json({ message: "ok" }, 200);
     }
 
+    await createStripeCustomer(stripe, db, payload.id, payload.email);
+    return c.json({ message: "ok" }, 200);
+  } catch (err: any) {
     if (err instanceof Error) {
       return c.json({ error: err.message }, 500);
     }
-
     return c.json({ error: "サーバーエラー" }, 500);
   }
 });
 
-// router.get("/checkout", async (c) => {
-//   const stripe = c.get("stripe");
+const checkoutSchema = z.object({
+  product_name: z.string(),
+  unit_amount: z.number().int().positive(),
+  quantity: z.number().int().positive(),
+});
 
-//   const session = await stripe.checkout.sessions.create({
-//     line_items: [{ price: "price_1TRsxE1lp8GZIfDzFJCFOPUQ", quantity: 1 }],
-//     mode: "payment",
-//     success_url: `http://127.0.0.1:8787/payment/success`,
-//   });
+router.post("/checkout", zValidator("json", checkoutSchema), async (c) => {
+  const { product_name, unit_amount, quantity } = c.req.valid("json");
+  const payload = c.get("jwtPayload");
+  const stripe = c.get("stripe");
+  const db = drizzle(c.env.DB!);
 
-//   return c.json({ session });
-// });
+  try {
+    const userPaymentRecord = await fetchUserPayment(db, payload.id);
+
+    if (!userPaymentRecord) {
+      return c.json({ error: "不正なユーザです" }, 404);
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      customer: userPaymentRecord.customer_id,
+      line_items: [
+        {
+          price_data: {
+            currency: "jpy",
+            product_data: { name: product_name },
+            unit_amount,
+          },
+          quantity,
+        },
+      ],
+      saved_payment_method_options: {
+        payment_method_save: "enabled",
+      },
+      mode: "payment",
+      success_url: `http://127.0.0.1:8787/payment/success`,
+    });
+
+    return c.json({ url: session.url });
+  } catch (err) {
+    if (err instanceof Stripe.errors.StripeError) {
+      return c.json({ error: err.message }, 400);
+    }
+    return c.json({ error: "サーバーエラー" }, 500);
+  }
+});
+
+async function fetchUserPayment(db: AppDatabase, user_id: string) {
+  return (await db
+    .select()
+    .from(user_payment_providers)
+    .where(eq(user_payment_providers.user_id, user_id))
+    .get()) as UserPaymentProvider | undefined;
+}
 
 async function createStripeCustomer(
   stripe: Stripe,
