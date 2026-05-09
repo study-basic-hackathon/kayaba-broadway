8fcefebd31853cb01ce2c25fa5eb3cbf6d098fb1
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2026-05-09 15:52:20 +0900

    livekitの情報を取得できるようなEndpointを追記した

--- src/routes/shops.ts
+++ src/routes/shops.ts
@@ -3,6 +3,7 @@ import { eq } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/d1';
 import { shops, products } from '../db/schema';
 import { type AppType } from '../types';
+import { AccessToken } from 'livekit-server-sdk';
 
 const router = new Hono<AppType>();
 
@@ -37,6 +38,25 @@ router.get('/:id/products', async (c) => {
   const productsRecord = await db.select().from(products).where(eq(products.shop_id, shop_id));
 
   return c.json({ products: productsRecord });
+});
+
+router.get("/:id/livekit/token", async (c) => {
+  const { id: shopId } = c.req.param();
+
+  const identity = "userId";
+
+  const apiKey    = c.env.LIVEKIT_API_KEY;
+  const apiSecret = c.env.LIVEKIT_API_SECRET;
+
+  // ApiKey/Secretは wrangler.toml の [vars] や Secrets から取得
+  const at = new AccessToken(apiKey, apiSecret, {identity: identity});
+
+  at.addGrant({ roomJoin: true, room: `shop-${shopId}`, canPublish: true, canSubscribe: true });
+
+  return c.json({
+    token: await at.toJwt(),
+    url: c.env.LIVEKIT_WS_URL // ここでURLも返すとフロントの管理が楽です
+  });
 });
 
 export default router;
