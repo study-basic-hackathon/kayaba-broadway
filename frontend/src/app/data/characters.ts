62ec48227473653dd5729ac5f63f56c0696d9317
hiyoccoder <201990519+hiyoccoder@users.noreply.github.com>
2026-05-10 15:08:13 +0900

    キャラクター規約見直して調整

--- frontend/src/app/data/characters.ts
+++ frontend/src/app/data/characters.ts
@@ -7,49 +7,9 @@ export interface Character {
 export const CHARACTERS: Character[] = [
   // 動物
   {
-    id: 'adelie-penguin',
-    name: 'アデリーペンギン',
-    image: '/assets/character/adelie-penguin.png',
-  },
-  {
-    id: 'cape-penguin',
-    name: 'ケープペンギン',
-    image: '/assets/character/cape-penguin.png',
-  },
-  {
-    id: 'emperor-penguin',
-    name: 'コウテイペンギン',
-    image: '/assets/character/emperor-penguin.png',
-  },
-  {
-    id: 'emperor-penguin-chick',
-    name: 'コウテイペンギン（幼鳥）',
-    image: '/assets/character/emperor-penguin-chick.png',
-  },
-  {
-    id: 'emperor-penguin-colony',
-    name: 'コウテイペンギン（群れ）',
-    image: '/assets/character/emperor-penguin-colony.png',
-  },
-  {
-    id: 'humboldt-penguin',
-    name: 'フンボルトペンギン',
-    image: '/assets/character/humboldt-penguin.png',
-  },
-  {
     id: 'penguin',
     name: 'ペンギン',
     image: '/assets/character/penguin.png',
-  },
-  {
-    id: 'shima-enaga',
-    name: 'シマエナガ',
-    image: '/assets/character/shima-enaga.png',
-  },
-  {
-    id: 'chibi-shima-enaga',
-    name: 'ちびシマエナガ',
-    image: '/assets/character/chibi-shima-enaga.png',
   },
   {
     id: 'sparrow',
@@ -57,109 +17,34 @@ export const CHARACTERS: Character[] = [
     image: '/assets/character/sparrow.png',
   },
   {
-    id: 'cat',
-    name: 'ねこ',
-    image: '/assets/character/cat.png',
-  },
-  {
-    id: 'rabbit',
-    name: 'うさぎ',
-    image: '/assets/character/rabbit.png',
-  },
-  {
     id: 'chicken',
-    name: 'にわとり',
+    name: 'ニワトリ',
     image: '/assets/character/chicken.png',
   },
   {
-    id: 'shrimp',
-    name: 'えび',
-    image: '/assets/character/shrimp.png',
+    id: 'chick',
+    name: 'ひよこ',
+    image: '/assets/character/chick.png',
   },
   {
-    id: 'mimic-octopus',
-    name: 'めんだこ',
-    image: '/assets/character/mimic-octopus.png',
+    id: 'gray-chick',
+    name: 'グレーひよこ',
+    image: '/assets/character/gray-chick.png',
   },
   {
-    id: 'starry-mimic-octopus',
-    name: 'ほしぞらめんだこ',
-    image: '/assets/character/starry-mimic-octopus.png',
+    id: 'green-chick',
+    name: 'グリーンひよこ',
+    image: '/assets/character/green-chick.png',
   },
 
   // ご飯
-  {
-    id: 'anpan',
-    name: 'あんぱん',
-    image: '/assets/character/anpan.png',
-  },
   {
     id: 'beer',
     name: 'ビール',
     image: '/assets/character/beer.png',
   },
-  {
-    id: 'cake',
-    name: 'ケーキ',
-    image: '/assets/character/cake.png',
-  },
-  {
-    id: 'croissant',
-    name: 'クロワッサン',
-    image: '/assets/character/croissant.png',
-  },
-  {
-    id: 'curry',
-    name: 'カレー',
-    image: '/assets/character/curry.png',
-  },
-  {
-    id: 'large-curry',
-    name: 'カレー大盛り',
-    image: '/assets/character/large-curry.png',
-  },
-  {
-    id: 'donut',
-    name: 'ドーナツ',
-    image: '/assets/character/donut.png',
-  },
-  {
-    id: 'maritozzo',
-    name: 'マリトッツォ',
-    image: '/assets/character/maritozzo.png',
-  },
-  {
-    id: 'omelette',
-    name: 'オムレツ',
-    image: '/assets/character/omelette.png',
-  },
-  {
-    id: 'omurice',
-    name: 'オムライス',
-    image: '/assets/character/omurice.png',
-  },
-  {
-    id: 'ramen',
-    name: 'ラーメン',
-    image: '/assets/character/ramen.png',
-  },
-  {
-    id: 'shokupan',
-    name: '食パン',
-    image: '/assets/character/shokupan.png',
-  },
-  {
-    id: 'teacup',
-    name: 'ティーカップ',
-    image: '/assets/character/teacup.png',
-  },
 
   // その他
-  {
-    id: 'ghost',
-    name: 'ゴースト',
-    image: '/assets/character/ghost.png',
-  },
   {
     id: 'moving-cardboard-box-avatar',
     name: '動く段ボール',
